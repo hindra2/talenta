1fa1344a308bf7c77ed9bdb7516ce9841bb4746e
Jonathan Bernard Widjajakusuma <jbw7@illinois.edu>
2024-01-30 16:28:42 -0600

    init dashboard-student

---
+++ src/app/dashboard-student/page.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>
+        <Link href="/dashboard-teachers">
+          <p>dashboard</p>
+        </Link>
+        <Link href="/login-page">
+          <p>login</p>
+        </Link>
+      </div>
+    </main>
+  );
+}
