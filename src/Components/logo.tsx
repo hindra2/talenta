0307fbf2df3f803ed9c662e4a5beea4650a8cf6f
H1R0L9 <53500251+H1R0L9@users.noreply.github.com>
2024-01-31 23:47:12 -0600

    added sidebar to teacher dashboard

---
+++ src/Components/logo.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import Image from "next/image";
+
+const Logo = () => {
+    return (    
+        <div className="flex items-center">
+            <Link href="/">
+                <Image
+                    src="/logo-talenta.png"
+                    alt="Talenta Logo"
+                    width={300}
+                    height={400}
+                />
+            </Link>
+        </div>
+    );
+};
+
+export default Logo;
