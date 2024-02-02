8ba199ec231572b06f4b510b98a489068674c3a9
H1R0L9 <53500251+H1R0L9@users.noreply.github.com>
2024-02-01 21:45:56 -0600

    added studentcard component

---
+++ src/Components/student-card.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import Image from "next/image";
+
+const Student_Card = () => {
+  return (
+    <div className="">
+
+    </div>
+  );
+};
+
+export default Student_Card;
