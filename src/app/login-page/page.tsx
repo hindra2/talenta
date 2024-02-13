b427e636c55baa8ebd97db17f667fb91e2fa4814
Jonathan Bernard Widjajakusuma <jbw7@illinois.edu>
2024-02-12 22:09:08 -0600

    lgoin page

--- src/app/login-page/page.tsx
+++ src/app/login-page/page.tsx
@@ -12,22 +12,22 @@ export default function Home() {
           </span>
         </div>
         <div>
-          <div className="flex w-[60%] h-[50px] w-[400px] bg-white shadow-lg rounded-2xl ml-[110px]">
-            <span className="ml-[35px]">EMAIL</span>
+          <div className="flex w-[60%] h-[50px] w-[400px] bg-white shadow-lg rounded-2xl ml-[110px] mt-[20px]">
+            <span className="ml-[40px] mt-[5px] font-bold text-xxxs text-headingBlack">EMAIL</span>
           </div>
-          <div className="flex w-[60%] h-[50px] w-[400px] mt-[20px] bg-white shadow-lg rounded-2xl ml-[110px]">
-            <span className="ml-[35px]">PASSWORD</span>
+          <div className="flex w-[60%] h-[50px] w-[400px] mt-[15px] bg-white shadow-lg rounded-2xl ml-[110px]">
+            <span className="ml-[40px] mt-[5px] font-bold text-xxxs text-headingBlack">PASSWORD</span>
           </div>
-          <div className="flex w-[60%] h-[30px] w-[120px] mt-[30px] bg-white shadow-lg rounded-3xl ml-[390px]">
-            <span className="ml-[35px] mt-[2px]"><center>LOGIN</center></span>
+          <div className="flex w-[60%] h-[30px] w-[110px] mt-[25px] bg-red-400 shadow-lg rounded-3xl ml-[390px]" >
+            <button className="ml-[33.5px] mt-[2px] font-bold text-sm text-white"><center>LOGIN</center></button>
           </div>
         </div>
-        <div className="w-[650px] text-1xl text-headingBlack">
+        <div className="w-[650px] text-1xl text-headingBlack mt-[40px]">
           <center>Only registered students and educators from partnered schools can access their accounts.</center>
 
-          <center>If you're unsure, find out <u>how it works</u>.</center>
+          <center>If you're unsure, find out <u className="font-bold">how it works</u>.</center>
         </div>
-        <div className="ml-[175px] w-[250px] h-[250px]">
+        <div className="ml-[245px] mt-[20px] w-[175px] h-[175px]">
           <Header /> { }
         </div>
       </div>
@@ -36,4 +36,3 @@ export default function Home() {
     </main>
   );
 }
-
