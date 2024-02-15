c48aad88197d9ee1a10c1c9ecbed0b447f26352d
Evan243 <evanmhuang@gmail.com>
2024-02-14 19:29:04 -0600

    homepage in progress

--- src/app/home-page/page.tsx
+++ src/app/home-page/page.tsx
@@ -3,7 +3,11 @@ export default function Home() {
     return (
         <main>
             <div>
-
+                <div className="flex min-h-screen flex-col items-center justify-between p-24">
+                    <span className="">
+                        Welcome to
+                    </span>
+                </div>
             </div>
         </main>
     )
