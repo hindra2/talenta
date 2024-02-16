1b781fede4fdcc0ee7a97fdb72d9d6df05fa9f0c
Evan243 <evanmhuang@gmail.com>
2024-02-16 15:56:57 -0600

    more progress

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,40 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="font-medium">
-        <Link href="/dashboard-teachers">
-          <p>dashboard-teachers</p>
-        </Link>
-        <Link href="/dashboard-student">
-          <p>dashboard-student</p>
-        </Link>
-        <Link href="/login-page">
-          <p>login</p>
-        </Link>
-        <Link href="/home-page">
-          <p>homepage</p>
-        </Link>
+    <main>
+      <div>
+        <div className="flex min-h-screen flex-col mt-[150px] ml-[175px] ">
+          <span className="text-3xl text-red-500">
+            Welcome to
+          </span>
+          <div className="ml-[-14px] mt-[0px]">
+            <Image src="/logo-talenta.png" alt="school logo" width={400} height={400} />
+          </div>
+          <span className=" text-subheadingBlack mt-[30px] text-sxl">
+            Are you a user? Access your account to explore & engage!
+          </span>
+          <div className="w-[500px]">
+            <Link href="login-page">
+              <button className="bg-red-400 rounded-3xl h-[40px] w-[145px] mt-[0px] items-center text-white">Login</button>
+            </Link>
+            <button className="bg-white rounded-3xl h-[40px] w-[145px] mt-[10px] ml-[25px] outline-4"><center className="text-gray-400">How It Works?</center>
+            </button>
+          </div>
+          <div className="w-[750px] mt-[100px]">
+            <span className="text-xl text-gray-800 font-bold">
+              Are you an institution?
+            </span>
+            <br></br>
+            <span className="text-subheadingBlack">
+              Revolutionize your educational experience and unlock the full potential of your students with our platform!
+              <br></br>
+              Explore <u className="font-bold">partnership</u> opportunities or <u className="font-bold">contact us for more information</u>
+            </span>
+          </div>
+        </div>
       </div>
     </main>
-  );
+  )
 }
-
