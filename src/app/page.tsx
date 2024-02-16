a04cbfc6dc68e4606ec6886572e177e3955b93e5
H1R0L9 <53500251+H1R0L9@users.noreply.github.com>
2024-02-16 16:05:20 -0600

    added placeholder buttons for dashboard-teacher and dashboard-student

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,6 +33,12 @@ export default function Home() {
               Explore <u className="font-bold">partnership</u> opportunities or <u className="font-bold">contact us for more information</u>
             </span>
           </div>
+          <Link href="dashboard-teachers">
+              <button className="bg-red-400 rounded-3xl h-[40px] w-[145px] mt-[0px] items-center text-white">dashboard teachers</button>
+          </Link>
+          <Link href="dashboard-student">
+              <button className="bg-red-400 rounded-3xl h-[40px] w-[145px] mt-[0px] items-center text-white">dashboard students</button>
+          </Link>
         </div>
       </div>
     </main>
