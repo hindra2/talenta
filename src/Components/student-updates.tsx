22d7c67d3b87cef5408d4ec425f850db2dd74ccd
Jonathan Bernard Widjajakusuma <jbw7@illinois.edu>
2024-02-16 18:56:49 -0600

    personal data component

--- src/Components/student-updates.tsx
+++ src/Components/student-updates.tsx
@@ -3,15 +3,16 @@ import Image from "next/image";
 
 const Student_Update = () => {
   return (
-    <div className="flex flex-col h-[100px] w-[300px] bg-white rounded-xl shadow-lg p-2 items-center">        
-        <div className="flex flex-col items-center">
-            <span>Anita Bath add 2 cetificates</span>
-        </div>
-        <div>
-            <button><span className="text-xxs">Just Now</span></button>
-            <span className="text-xxs"> | </span>
-            <button><span className="text-xxs">Open</span></button>
-        </div>
+    <div className="flex flex-col h-[100px] w-full bg-white rounded-xl shadow-lg p-2 items-center">        
+      <div className="bgcolor-black"></div>
+      <div className="flex flex-col items-center">
+        <span>Anita Bath add 2 cetificates</span>
+      </div>
+      <div>
+        <button><span className="text-xxs">Just Now</span></button>
+        <span className="text-xxs"> | </span>
+        <button><span className="text-xxs">Open</span></button>
+      </div>
     </div>
   );
 };
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,9 @@
 import type { Metadata } from "next";
+import { Montserrat } from "next/font/google";
 import "./globals.css";
+
+const inter = Montserrat({ subsets: ["latin"] });
+
 
 export const metadata: Metadata = {
   title: "Talenta",
