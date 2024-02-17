d10793e09c617b7a01eb7af7b42ffe12706772c5
Jonathan Bernard Widjajakusuma <jbw7@illinois.edu>
2024-02-16 20:04:39 -0600

    updated container

--- src/Components/student-updates.tsx
+++ src/Components/student-updates.tsx
@@ -3,17 +3,20 @@ import Image from "next/image";
 
 const Student_Update = () => {
   return (
-    <div className="flex flex-col h-[100px] w-full bg-white rounded-xl shadow-lg p-2 items-center">        
-      <div className="bgcolor-black"></div>
-      <div className="flex flex-col items-center">
-        <span>Anita Bath add 2 cetificates</span>
+      <div className="flex items-center h-[75px] w-full bg-white shadow-md rounded-xl">
+        <div className="h-full w-2 bg-headingRed rounded-l-lg">
+        </div>
+        <div className="ml-2 items-center">
+          <div>
+            <span>Anita Bath add 2 cetificates</span>
+          </div>
+          <div>
+            <button><span className="text-xxs">Just Now</span></button>
+              <span className="text-xxs"> | </span>
+            <button><span className="text-xxs">Open</span></button>
+          </div>
+        </div>
       </div>
-      <div>
-        <button><span className="text-xxs">Just Now</span></button>
-        <span className="text-xxs"> | </span>
-        <button><span className="text-xxs">Open</span></button>
-      </div>
-    </div>
   );
 };
 
