476f454fafda1c3d8952b7429e8f7f1cc2135722
H1R0L9 <53500251+H1R0L9@users.noreply.github.com>
2024-02-16 21:18:51 -0600

    updated next config

---
+++ out/_next/static/chunks/app/login-page/layout-b22558c1e446446a.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[626],{4442:function(n,a,e){Promise.resolve().then(e.t.bind(e,3513,23))},3513:function(n){n.exports={style:{fontFamily:"'__Inter_aaf875', '__Inter_Fallback_aaf875'",fontStyle:"normal"},className:"__className_aaf875"}}},function(n){n.O(0,[971,69,744],function(){return n(n.s=4442)}),_N_E=n.O()}]);
